7a077d566339cddc820854244f62184076957ea2
해준 <127281057+ftery0@users.noreply.github.com>
2024-09-16 18:17:18 +0900

    init

---
+++ src/main/java/hj/Interest/Existence/InterestExistenceApplication.java
@@ -0,0 +1,13 @@
+package hj.Interest.Existence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterestExistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterestExistenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hj/Interest/Existence/InterestExistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package hj.Interest.Existence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterestExistenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
